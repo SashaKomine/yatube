08e0539fb09c88f0af7b49c5e1c38d2ba465838a
Саша Айдарова <sasyayan@yandex.by>
2022-08-04 16:35:28 +0800

    with pytest fix tests

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,9 +1,12 @@
+from xml.sax import handler
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+handler403 = 'core.views.permission_denied'
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='index'),
